425aaac46d1b953b168b1c1405beb8f6b9f32754
TRSLimey <trslimey@osdlt.Home>
2019-12-27 14:15:43 +0000

    Code

---
+++ app/src/main/java/pl/trslimey/HWClick/MainActivity.kt
@@ -0,0 +1,45 @@
+package pl.trslimey.HWClick
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Show toast warning
+        Toast.makeText(this@MainActivity, "WARNING: This app is experimental and created for learning purposes!", Toast.LENGTH_LONG).show()
+    }
+
+    fun onButtonClick(view: android.view.View) {
+        // Increment counter
+        counter += 1
+        // Prepare new counter string
+        var counter_string: String = counter.toString()
+        if (counter == 1) {
+            counter_string += " Click"
+        } else {
+            counter_string += " Clicks"
+        }
+        if (counter == 69) {
+            counter_string += " (Nice)"
+        }
+        // Set counter text box to this string
+        txt_counter.text = counter_string
+        // Also randomly change counter color
+        var colorCode: String = "#"
+        for (i in 1..6) {
+            colorCode += Random(counter*i).nextInt(0,16).toString(16)
+        }
+        //txt_counter.setTextColor(Color.parseColor(color_code))
+        background.setBackgroundColor(Color.parseColor(colorCode))
+        btn_clicker.setBackgroundColor(Color.parseColor(colorCode))
+    }
+}
